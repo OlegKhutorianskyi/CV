54e9c315168d0b3eaccb940931353c01d69307d2
Oleg Khutorianskiy <compilated510@outlook.com>
2023-04-19 01:15:37 +0200

    initial commit

---
+++ js/localisation.js
@@ -0,0 +1,28 @@
+const langArr = {
+    'name': {
+        'ua' : 'Хуторянський Олег Віталійович',
+        'ru' : 'Хуторянский Олег Виталиевич',
+        'en' : 'Khutorianskiy Oleg',
+        'de' : 'Khutorianskiy Oleg'
+    },
+    'welocome': {
+        'ua' : 
+        'Ласкаво прошу на онлайн версію мого резюме! Тут ви детально ознайометесь про мої: технічні навички soft skills, досвід роботі, освіту та мої контакти',
+        'ru' : 
+        'Добро пожаловать в онлайн-версию моего резюме! здесь вы подробно узнаете о моем: технические навыки, soft skills, опыт работы и образование',
+        'en' : 'Welcome to the online version of my resume! Here you will get to know in detail about me: technical skills, soft skills, work experience, education and my contacts',
+        'de' : 'Willkommen zur Online-Version meines Lebenslaufs! Hier erfahren Sie im Detail etwas über mich: Fachkompetenz, Soft Skills, Berufserfahrung, Ausbildung und meine Kontakte'
+    },
+    'projects': {
+        'ua' : 'Проекти',
+        'ru' : 'Проекты',
+        'en' : 'Projects',
+        'de' : 'Projekte'
+    },
+    'projects': {
+        'ua' : 'Проекти',
+        'ru' : 'Проекты',
+        'en' : 'Projects',
+        'de' : 'Projekte'
+    },
+}
